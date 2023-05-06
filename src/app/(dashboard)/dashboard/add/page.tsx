1b699fd90efb4440120b177c80aee60dd54701cc
Hubert Madej <hubertmadej02@gmail.com>
2023-05-06 19:22:27 +0200

    Implement adding friends functionality

---
+++ src/app/(dashboard)/dashboard/add/page.tsx
@@ -0,0 +1,11 @@
+import AddFriendForm from '@/components/ui/AddFriendForm'
+import { FC } from 'react'
+
+const page: FC = ({}) => {
+  return <main className='pt-8'>
+    <h1 className='font-bold text-5xl mb-8'>Add a friend</h1>
+    <AddFriendForm />
+  </main>
+}
+
+export default page
