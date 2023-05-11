b3ef83644c3ade63a993012ebbc5be069b424d74
Hubert Madej <hubertmadej02@gmail.com>
2023-05-11 10:45:56 +0200

    Fix hydration error

--- src/app/(dashboard)/dashboard/chat/[chatId]/page.tsx
+++ src/app/(dashboard)/dashboard/chat/[chatId]/page.tsx
@@ -1,8 +1,9 @@
+"use client";
+
 import ChatInput from "@/components/ChatInput";
 import Messages from "@/components/Messages";
 import { fetchRedis } from "@/helpers/redis";
 import { authOptions } from "@/lib/auth";
-import { database } from "@/lib/database";
 import { messageArrayValidator } from "@/lib/validations/message";
 import { getServerSession } from "next-auth";
 import Image from "next/image";
